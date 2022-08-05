5edcb474f6a49dca250b794b4e6cd089f65f761a
goodbyeFlash <goodbyeflash.dev@gmail.com>
2022-08-05 23:47:12 +0900

    텍스쳐 적용 소스 추가

--- public/MetaOctagon3DViewer/js/script.js
+++ public/MetaOctagon3DViewer/js/script.js
@@ -5,6 +5,8 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 const clock = new THREE.Clock();
 
+//scene.add(new THREE.AxesHelper(5));
+
 let mixer;
 
 /**
@@ -33,11 +35,40 @@ const directionalLight = new THREE.DirectionalLight(
   lightState.directIntensity
 );
 
+// const pointLight = new THREE.PointLight(0xff9000, 0.9, 15, 3);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+
+// const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 30, Math.PI * 0.1, 0.1, 1);
+// spotLight.position.set(0, 2, 3);
+// scene.add(spotLight);
+
+// spotLight.target.position.x = -0.75;
+// scene.add(spotLight.target);
+
+// const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 10, 5);
+// rectAreaLight.position.set(-1.5, 0, 1.5);
+// rectAreaLight.lookAt(new THREE.Vector3());
+
+// Light Helpers
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+//   hemisphereLight,
+//   0.2
+// );
+// scene.add(hemisphereLightHelper);
+
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+
+
 directionalLight.position.x = 2;
 
+//scene.add(rectAreaLight);
 scene.add(hemisphereLight);
 scene.add(ambientLight);
 scene.add(directionalLight);
+
 
 /**
  * Renderer
@@ -50,8 +81,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.physicallyCorrectLights = true;
-// renderer.outputEncoding = true;
-// renderer.outputEncoding = THREE.sRGBEncoding();
+renderer.shadowMap.enabled = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
 
 const tick = () => {
   // Update Orbital Controls
@@ -63,6 +94,8 @@ const tick = () => {
 
   // Render
   renderer.render(scene, camera);
+
+  //spotLightHelper.update();
 
   if (camera.position.x >= 50) {
     camera.position.x -= 0.01;
@@ -77,37 +110,54 @@ const tick = () => {
 // GLTF
 const gltfLoader = new THREE.GLTFLoader();
 gltfLoader.load(
-  './models/0803.glb',
+  './models/Octafinal.gltf',
   (gltf) => {
-    const model = gltf.scene;
+    const model = gltf.scene;    
+    model.position.y = -18;
+
+    //mixer = new THREE.AnimationMixer(model);
+
+    //const clips = gltf.animations;
+
+    // clips.forEach((clip) => {
+    //   const liTag = document.createElement('li');      
+    //   liTag.innerText = clip.name;
+    //   liTag.style.cursor = 'pointer';
+    //   liTag.onclick = (e) => {
+    //     // 특정 애니메이션 재생
+    //     const clip = THREE.AnimationClip.findByName(clips, e.target.innerText);
+    //     const action = mixer.clipAction(clip);
+    //     action.play();
+    //   };
+    // });
+
+    // model.traverse(function (child) {      
+      
+    //   if (child.isMesh) {
+    //       const m = child;
+    //       m.receiveShadow = true;
+    //       m.castShadow = true;
+    //   }
+    //   if (child.isLight) {
+    //       const l = child;
+    //       l.castShadow = true;
+    //       l.shadow.bias = -0.003;
+    //       l.shadow.mapSize.width = 2048;
+    //       l.shadow.mapSize.height = 2048;
+    //   }     
+    // });
+
     scene.add(model);
 
-    model.position.y = -15;
-
-    mixer = new THREE.AnimationMixer(model);
-    const clips = gltf.animations;
-
-    clips.forEach((clip) => {
-      const liTag = document.createElement('li');
-      liTag.innerText = clip.name;
-      liTag.style.cursor = 'pointer';
-      liTag.onclick = (e) => {
-        // 특정 애니메이션 재생
-        const clip = THREE.AnimationClip.findByName(clips, e.target.innerText);
-        const action = mixer.clipAction(clip);
-        action.play();
-      };
-    });
-
-    const btnTag = document.createElement('Button');
-    btnTag.innerText = 'reset';
-    btnTag.onclick = (e) => {
-      mixer.stopAllAction();
-    };
+    // const btnTag = document.createElement('Button');
+    // btnTag.innerText = 'reset';
+    // btnTag.onclick = (e) => {
+    //   mixer.stopAllAction();
+    // };
 
     tick();    
-
     sendMsgToParent("gltfLoading");
+
   },
   undefined,
   (error) => {
@@ -122,8 +172,8 @@ gltfLoader.load(
 const camera = new THREE.PerspectiveCamera(
   20,
   sizes.width / sizes.height,
-  75,
-  2000
+  70,
+  1000
 );
 camera.position.x = 100;
 camera.position.y = 0;
